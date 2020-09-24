0a056d543241dea28bd9e5a307199ea7df76e2b2
Bruno Martins <bgcngm@gmail.com>
2020-09-24 11:51:33 +0100

    build: Use $(HOST_PREBUILT_TAG) where possible
    Change-Id: If211d23ce72b3404368cd723cbb9ea801e61802c
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/tasks/pixeldust.mk
+++ build/tasks/pixeldust.mk
@@ -18,7 +18,7 @@
 
 PD_TARGET_PACKAGE := $(PRODUCT_OUT)/$(PIXELDUST_VERSION).zip
 
-MD5 := prebuilts/build-tools/path/$(HOST_OS)-x86/md5sum
+MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
 
 .PHONY: pixeldust
 pixeldust: $(INTERNAL_OTA_PACKAGE_TARGET)
