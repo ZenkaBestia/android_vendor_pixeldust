b389ed8c2fe33d555bbd1a278969bf1100b9d694
Pig <pig.priv@gmail.com>
2020-10-15 02:58:37 +0800

    soong: Add pixeldust soong config variables.
    Ref: https://android.googlesource.com/platform/build/soong/+/refs/tags/android-11.0.0_r4#soong-config-variables
    
    spezi77: Stripped down to only what is absolutely necessary for us atm
    Change-Id: Icaf35b65b63d4245c92e39ce628bb8d6ffd0d4bb
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -14,6 +14,7 @@ EXPORT_TO_SOONG := \
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
 
+# Pixeldust variables
 SOONG_CONFIG_NAMESPACES += pixeldustVarsPlugin
 
 SOONG_CONFIG_pixeldustVarsPlugin :=
@@ -24,3 +25,20 @@ define addVar
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+
+SOONG_CONFIG_NAMESPACES += pixeldustGlobalVars
+SOONG_CONFIG_pixeldustGlobalVars += \
+    target_init_vendor_lib \
+    target_ld_shim_libs \
+    target_process_sdk_version_override \
+    target_surfaceflinger_fod_lib
+
+# Set default values
+TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
+
+# Soong value variables
+SOONG_CONFIG_pixeldustGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_pixeldustGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
+SOONG_CONFIG_pixeldustGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
+SOONG_CONFIG_pixeldustGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
