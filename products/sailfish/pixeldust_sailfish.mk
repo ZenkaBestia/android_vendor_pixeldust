b8915513d7ba7ab628bf331caf1cf0d47d743f8c
spezi77 <spezi7713@gmx.net>
2021-02-18 21:13:18 +0100

    marlin/sailfish: Don't build google's NetworkStack. We cannot use Google's apex on this device. Therefore exclude them from gapps: - NetworkPermissionConfigGoogle - NetworkStackGoogle
    Also read this: https://github.com/PixelDust-Eleven/android_vendor_pixeldust/commit/13cb47a67e33a65468ea2ccfa72a87716b3524c3
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- products/sailfish/pixeldust_sailfish.mk
+++ products/sailfish/pixeldust_sailfish.mk
@@ -55,6 +55,8 @@ REMOVE_GAPPS_PACKAGES += \
     GoogleFeedback \
     GoogleTTS \
     Maps \
+    NetworkPermissionConfigGoogle \
+    NetworkStackGoogle \
     NgaResources \
     PixelLiveWallpaperPrebuilt \
     pixel_experience_2019_midyear \
